b5834eee474c1e16d325d5611606f7fa65b49cad
Aguynamedkent <butaya.kentvincent07@gmail.com>
2026-01-17 17:30:50 +0800

    add multiplication function to calculator py

--- Calculator.py
+++ Calculator.py
@@ -16,6 +16,10 @@ def addition(num1, num2):
 def subtraction(num1, num2):
     difference = num1 - num2
     print(f"\n\nThe difference of {num1} and {num2} is {difference}.\n\n")
+    
+def multiplication(num1, num2):
+    product = num1 * num2
+    print(f"\n\nThe product of {num1} and {num2} is {product}.\n\n")
 
 while(True):
     print("This is a simple floating point integer calculator")
@@ -39,6 +43,9 @@ while(True):
             subtraction(num1, num2)
             continue
         case 3:
+            num1 = get_input("Input the first number: ")
+            num2 = get_input("Input the second number: ")
+            multiplication(num1, num2)
             continue
         case 4:
             continue
