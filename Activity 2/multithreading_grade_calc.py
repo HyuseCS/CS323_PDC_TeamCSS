f2916c0a29bfc5535176cc17548a7417f196896f
seodowa <pagalan.theodoreustp@gmail.com>
2026-02-07 18:11:52 +0800

    Create functionality for multithreading

--- Activity 2/multithreading_grade_calc.py
+++ Activity 2/multithreading_grade_calc.py
@@ -1,16 +1,47 @@
 import threading
 
+FINAL_GWA = 0
+LOCK = threading.Lock()
+
 def compute_gwa(grades):
+    global FINAL_GWA
+    
     gwa = sum(grades) / len(grades)
+    
     print(f"[Thread] Calculated GWA: {gwa}")
 
-grades_list = [85, 90, 78, 92] # Replace with user input
+    with LOCK:
+        FINAL_GWA = gwa
 
-threads = []
-for grade in grades_list:
-    t = threading.Thread(target=compute_gwa, args=([grade],))
-    threads.append(t)
-    t.start()
+# Ensure the input is a number
+def get_number(input_text):
+    while True:
+        try:
+            number = int(input(input_text))
+            break
+        except (TypeError, ValueError):
+            pass
+    return number
 
-for t in threads:
-    t.join()
+def main():
+    grades_list = [] 
+
+    grades_count = get_number("Number of grades: ")
+    threads = []
+    
+    for _ in range(grades_count):
+        grades_list.append(get_number("Enter grade: "))
+        t = threading.Thread(target=compute_gwa, args=(grades_list,))
+        threads.append(t)
+    
+    for t in threads:
+        t.start()
+
+    for t in threads:
+        t.join()
+
+    print("Final GWA:", FINAL_GWA) 
+
+
+if __name__ == "__main__":
+    main()
