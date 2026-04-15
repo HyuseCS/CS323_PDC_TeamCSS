4d38815c433fd13c6ffbcdfbcb2bc080edbef392
seodowa <pagalan.theodoreustp@gmail.com>
2026-04-15 20:52:21 +0800

    add delay

--- Final_FirstLab/main.py
+++ Final_FirstLab/main.py
@@ -1,6 +1,6 @@
 import multiprocessing
 import random
-import sys
+import time
 
 # The Goal: Use a Manager list for shared memory without freezing MPI.
 # We initialize inside the __main__ block to ensure clean process separation.
@@ -10,6 +10,8 @@ if __name__ == '__main__':
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
     size = comm.Get_size()
+    manager = multiprocessing.Manager()
+    shared_orders = manager.list()
 
     ITEMS = [
         "Mechanical Keyboard (75% Layout)",
@@ -21,19 +23,16 @@ if __name__ == '__main__':
     # Rank 0 (Master) manages the shared list
     if rank == 0:
         print(f"Master (Rank 0): Starting Manager Server...")
-        manager = multiprocessing.Manager()
-        shared_orders = manager.list()
         print(f"Master (Rank 0): Manager is ready.")
         
         try:
-            # 1. Distribute 8 orders to workers
+            # Distribute 8 orders to workers
             for i in range(8):
                 worker_id = (i % (size - 1)) + 1
                 order = {'order_id': i, 'item_name': random.choice(ITEMS)}
                 comm.send(order, dest=worker_id)
 
-            # 2. Collect reports back from workers and store in shared memory
-            # (add lock here)
+            # Collect reports back from workers and store in shared memory
             print(f"Master (Rank 0): Waiting for results...")
             for i in range(8):
                 processed_data = comm.recv(source=MPI.ANY_SOURCE)
@@ -46,7 +45,7 @@ if __name__ == '__main__':
             for worker_id in range(1, size):
                 comm.send("TERMINATE", dest=worker_id)
 
-            # 3. Final output from the shared Manager structure
+            
             print("\n--- Final Completed Orders in Shared Memory ---")
             for completed_order in shared_orders:
                 print(completed_order)
@@ -57,7 +56,7 @@ if __name__ == '__main__':
             print("Master (Rank 0): Manager shut down successfully.")
             
     else:
-        # Worker logic (Ranks 1, 2, 3...)
+        # Worker logic 
         while True:
             data = comm.recv(source=0)
             if data == "TERMINATE":
@@ -65,7 +64,10 @@ if __name__ == '__main__':
             
             print(f"Worker {rank} processing item {data['item_name']} (Order ID: {data['order_id']})")
             
-            #(Add delay here)
+            # delay
+            delay = random.randint(1, 3)
+            time.sleep(delay)  
+            print(f"Worker {rank} finished processing item {data['item_name']} (Order ID: {data['order_id']}) (Time elapsed: {delay} seconds)")
             
             # Sends results back to Master to be stored in the Manager list
             comm.send(data, dest=0)
