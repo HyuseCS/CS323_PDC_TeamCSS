ee32e1fef58f427cfc6cf9259d0d8f3f195e19af
Kent Vincent Butaya <butaya.kentvincent07@gmail.com>
2026-04-17 12:54:46 +0800

    chore: clean up code

--- Final_FirstLab/main.py
+++ Final_FirstLab/main.py
@@ -2,9 +2,6 @@ import multiprocessing
 import random
 import time
 
-# The Goal: Use a Manager list for shared memory without freezing MPI.
-# We initialize inside the __main__ block to ensure clean process separation.
-
 def main(comm, rank, size, manager, shared_orders):
     # Rank 0 (Master) manages the shared list
     if rank == 0:
@@ -12,13 +9,11 @@ def main(comm, rank, size, manager, shared_orders):
         print(f"Master (Rank 0): Manager is ready.")
         
         try:
-            # Distribute 8 orders to workers
             for i in range(8):
                 worker_id = (i % (size - 1)) + 1
                 order = {'order_id': i, 'item_name': random.choice(ITEMS)}
                 comm.send(order, dest=worker_id)
 
-            # Collect reports back from workers and store in shared memory
             print(f"Master (Rank 0): Waiting for results...")
             for i in range(8):
                 processed_data = comm.recv(source=MPI.ANY_SOURCE)
@@ -26,7 +21,6 @@ def main(comm, rank, size, manager, shared_orders):
                 shared_orders.append(processed_data)
                 print(f"Master (Rank 0): Progress {i+1}/8")
 
-            # Signal workers to finish
             for worker_id in range(1, size):
                 comm.send("TERMINATE", dest=worker_id)
 
@@ -36,7 +30,6 @@ def main(comm, rank, size, manager, shared_orders):
                 print(completed_order)
                 
         finally:
-            # Explicitly shut down the manager to release the port/socket
             manager.shutdown()
             print("Master (Rank 0): Manager shut down successfully.")
             
@@ -49,12 +42,10 @@ def main(comm, rank, size, manager, shared_orders):
             
             print(f"Worker {rank} processing item {data['item_name']} (Order ID: {data['order_id']})")
             
-            # delay
             delay = random.randint(1, 3)
             time.sleep(delay)  
             print(f"Worker {rank} finished processing item {data['item_name']} (Order ID: {data['order_id']}) (Time elapsed: {delay} seconds)")
             
-            # Sends results back to Master to be stored in the Manager list
             comm.send(data, dest=0)
 
 
